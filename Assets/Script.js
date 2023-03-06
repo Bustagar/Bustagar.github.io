72c2b8f02b0897d6a5907fbd6183fbcdba963fa9
Bustagar <sebastian.bustamantegarcia@gmail.com>
2023-03-06 17:34:01 -0300

    first commit

---
+++ Assets/Script.js
@@ -0,0 +1,16 @@
+let myIconFacebook = document.querySelector('.facebook');
+let myIconLinkedin = document.querySelector('.linkedin');
+let myIconWhatsapp = document.querySelector('.whatsapp');
+
+
+myIconFacebook.addEventListener('click', function windowOpen () {
+    window.open('https://www.facebook.com/sebabusgar', '_blank')
+});
+
+myIconLinkedin.addEventListener('click', function windowOpen () {
+    window.open('https://www.linkedin.com/in/alef-tavsebastianbustamante/', '_blank')
+});
+
+myIconWhatsapp.addEventListener('click', function windowOpen () {
+    window.open('https://wa.me/5492615920917', '_blank')
+});
